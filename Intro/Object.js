6b04da6999aeb4054683cf3043118dc9c02ae4a1
Akshay A <adominic21@gmail.com>
2021-06-18 23:21:05 +0530

    getCard funnction is added

--- Intro/Object.js
+++ Intro/Object.js
@@ -34,3 +34,34 @@ function isPangram(sentence){
     }
     return true;
 }
+
+function getCard(){
+    const value = [1,2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
+    const suits = ["clubs","spades","hearts","diamonds"];
+    let randValue = Math.floor(Math.random()*value.length);
+    let randsuits = Math.floor(Math.random()*suits.length);
+
+    let obj = {
+        value : value[randValue],
+        suit : suits[randsuits]
+    };
+    return obj;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
