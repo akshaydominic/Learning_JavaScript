17028eaf62e36cbc6236308faba7c36d25631fc7
Akshay A <adominic21@gmail.com>
2021-06-19 17:15:56 +0530

    added find func

--- Intro/functions.js
+++ Intro/functions.js
@@ -1,4 +1,5 @@
 const arr =[7,3,13,11,10,20,30,40,50];
+const words = ['asap','byob','rsvp','diy'];
 
 //forEach
 // arr.forEach(n=>(console.log(n*2)));
@@ -18,3 +19,13 @@ let mapObject = arr.map(n=>{
         isEven:n%2==0
     };
 });
+
+let abbrev = words.map(string=>{
+    return string.toUpperCase().split('').join(".");
+});
+
+//find
+let foundWord = words.find(string=>(
+    string.includes('rs')
+));
+
