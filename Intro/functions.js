ad2615ef2d76d635fcf8691a9a0c3161cb6d4b1d
Akshay A <adominic21@gmail.com>
2021-06-19 17:08:19 +0530

    Map

--- Intro/functions.js
+++ Intro/functions.js
@@ -1,2 +1,20 @@
-const arr =[10,20,30,40,50];
-arr.forEach(n=>(console.log(n*2)));
+const arr =[7,3,13,11,10,20,30,40,50];
+
+//forEach
+// arr.forEach(n=>(console.log(n*2)));
+
+//Map
+let mapArray =arr.map(n=>(n*0.5));
+
+let mapArray1 = arr.map((n)=>{
+    if(n%2==0)
+        return n;
+    return 0;
+});
+
+let mapObject = arr.map(n=>{
+    return {
+        value:n,
+        isEven:n%2==0
+    };
+});
